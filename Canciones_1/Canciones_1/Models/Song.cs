f4d23ccad9c64b299fd6468ab7d05ee7c353f596
GerardoLeon17 <60556189+GerardoLeon17@users.noreply.github.com>
2020-04-29 21:19:34 -0700

    Add files via upload

---
+++ Canciones_1/Canciones_1/Models/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Canciones_1.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Artist { get; set; }
+
+        public string SongName { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        public string Duration { get; set; }
+
+
+    }
+}
---
+++ Canciones_1/Canciones_1/Models/SongDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Canciones_1.Models
+{
+    public class SongDbContext: DbContext
+    {
+        public SongDbContext():base("MyConnection")
+
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+
+    }
+}
